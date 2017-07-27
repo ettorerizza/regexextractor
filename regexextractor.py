87add01e328f90462265196053911a9e6bd2f7af
alexgonca <alexandre.reportagem@gmail.com>
2017-07-26 17:36:40 -0700

    fix return objects

--- regexextractor.py
+++ regexextractor.py
@@ -15,19 +15,26 @@ class Importable:
         regex = wf_module.get_param_string('expression')
         newcol = wf_module.get_param_string('newcolumn')
 
-        try:
-            re.compile(regex)
-            is_valid = True
-        except re.error:
-            is_valid = False
-
-        if not is_valid:
-            wf_module.set_error('Invalid regular expression.')
+        if col == '' or regex == '' or newcol == '':
+            wf_module.set_ready(notify=False)
             return None
-        elif re.compile(regex).groups != 1:
-            wf_module.set_error('You need to specify one (and only one) capture group in your regular expression.')
+        elif col not in table.columns:
+            wf_module.set_error('Invalid column.')
             return None
         else:
-            table[newcol] = table[col].str.extract(regex, expand=True)
-            wf_module.set_ready(notify=False)
-            return table
+            try:
+                re.compile(regex)
+                is_valid = True
+            except re.error:
+                is_valid = False
+
+            if not is_valid:
+                wf_module.set_error('Invalid regular expression.')
+                return None
+            elif re.compile(regex).groups != 1:
+                wf_module.set_error('You need to specify one (and only one) capture group in your regular expression.')
+                return None
+            else:
+                table[newcol] = table[col].str.extract(regex, expand=True)
+                wf_module.set_ready(notify=False)
+                return table
