2d051a05eee3c01036b942d623ec8c200e25b975
Adam Hooper <adam@adamhooper.com>
2018-12-19 10:16:11 -0500

    Simplify unit tests; never raise exceptions
    Also, simplify `.str` handling. So much less code now!
    
    Fixes error on production:
    
    Traceback (most recent call last):
      File "/app/server/models/loaded_module.py", line 185, in render
        out = self.render_impl(arg1, arg2, **kwargs)
      File "/app/importedmodules/regexextractor/51c02e3/regexextractor.py", line 24, in render
        raise ValueError('You need to specify one (and only one) capture group in your regular expression.')
    ValueError: You need to specify one (and only one) capture group in your regular expression.

--- regexextractor.py
+++ regexextractor.py
@@ -1,31 +1,38 @@
+import pandas as pd
+import re
+
+
 def render(table, params):
     col = params['column']
     regex = params['expression']
     newcol = params['newcolumn']
 
-    if col == '' or regex == '' or newcol == '':
+    if not col or not regex or not newcol:
         return table
-    else:
-        try:
-            re.compile(regex)
-            is_valid = True
-        except re.error:
-            is_valid = False
 
-        if not is_valid:
-            raise ValueError('Invalid regular expression.')
-        elif re.compile(regex).groups != 1:
-            raise ValueError('You need to specify one (and only one) capture group in your regular expression.')
-        else:
-            # Non-object types cast string "NaN" when None, remove first
-            if table[col].dtype.name != 'category' and table[col].dtype.name != 'object':
-                table[newcol] = table[col].fillna('').astype(str).str.extract(regex, expand=True)
-            # Same issue with 'int' categorical dtypes, must add '' to categories (copy) first
-            elif table[col].dtype.name == 'category':
-                if any(pd.isna(table[col])):
-                    table[newcol] = table[col].copy().cat.add_categories('').fillna('').astype(str).str.extract(regex, expand=True)
-                else:
-                    table[newcol] = table[col].astype(str).str.extract(regex, expand=True)
-            else:
-                table[newcol] = table[col].str.extract(regex, expand=True)
-            return table
+    try:
+        compiled = re.compile(regex)
+    except re.error as err:
+        return 'Invalid regex: ' + str(err)
+
+    if not compiled.groups:
+        return 'Your regex needs a capture group. Add (parentheses) around it.'
+    elif compiled.groups > 1:
+        return (
+            'Workbench only supports one (capture group). '
+            'Remove some parentheses.'
+        )
+
+    series = table[col]
+
+    # Ensure series is str, overwriting if needed
+    # TODO [adamhooper, 2018-12-19] delete this feature: require str input
+    try:
+        series.str
+    except AttributeError:
+        strs = series.astype(str)
+        strs[series.isna()] = None
+        series = strs
+
+    table[newcol] = series.str.extract(compiled, expand=False)
+    return table
--- test_regexextractor.py
+++ test_regexextractor.py
@@ -1,99 +1,134 @@
 import unittest
 import pandas as pd
+from pandas.testing import assert_frame_equal
 import numpy as np
 from regexextractor import render
 
+
 class TestRegexExtractor(unittest.TestCase):
+    def test_defaults_nop(self):
+        out = render(pd.DataFrame({'A': [1, 2]}), {
+            'column': '',
+            'expression': '',
+            'newcolumn': ''
+        })
+        assert_frame_equal(out, pd.DataFrame({'A': [1, 2]}))
 
-    def setUp(self):
-        # Test data includes:
-        #  - rows of numeric and string types
-        #  - zero entries (which should not be removed)
-        #  - some partially and some completely empty rows
-        self.table = pd.DataFrame([
-            ['workbench!!!!',      2,      3.14,       '@workbench',     1.0],
-            ['.workbench!',    5,		None,		'.workbench!',    2.0],
-            ['[workbench]', -10, 	None, 	    '$data!',    None],
-            ['workbench',        -2,		10.0,			'data!!!',     3.0],
-            ['@workbench.com',		8,		0.0,			'@workbench.com',    4.0]],
-            columns=['stringcol','intcol','floatcol','catcol','floatcatcol'])
+    def test_no_column_nop(self):
+        out = render(pd.DataFrame({'A': [1, 2]}), {
+            'column': '',
+            'expression': r'(\w)',
+            'newcolumn': 'B'
+        })
+        assert_frame_equal(out, pd.DataFrame({'A': [1, 2]}))
 
-        # Pandas should infer these types anyway, but leave nothing to chance
-        self.table['stringcol'] = self.table['stringcol'].astype(str)
-        self.table['intcol'] = self.table['intcol'].astype(np.int64)
-        self.table['floatcol'] = self.table['floatcol'].astype(np.float64)
-        self.table['catcol'] = self.table['catcol'].astype('category')
-        self.table['floatcatcol'] = self.table['floatcatcol'].astype('category')
+    def test_no_newcolumn_nop(self):
+        out = render(pd.DataFrame({'A': [1, 2]}), {
+            'column': 'A',
+            'expression': r'(\w)',
+            'newcolumn': ''
+        })
+        assert_frame_equal(out, pd.DataFrame({'A': [1, 2]}))
 
-    def test_NOP(self):
-        params = { 'column': '', 'expression': '[^\w\s]', 'newcolumn': 'newcol'}
-        out = render(self.table, params)
-        self.assertTrue(out.equals(self.table)) # should NOP when first applied
+    def test_no_expression_nop(self):
+        out = render(pd.DataFrame({'A': ['a', 'b']}), {
+            'column': 'A',
+            'expression': '',
+            'newcolumn': ''
+        })
+        assert_frame_equal(out, pd.DataFrame({'A': ['a', 'b']}))
 
-        params = {'column': 'stringcol', 'expression': '[^\w\s]', 'newcolumn': ''}
-        out = render(self.table, params)
-        self.assertTrue(out.equals(self.table))  # should NOP when first applied
+    def test_string_match(self):
+        out = render(pd.DataFrame({'A': ['abc', 'def']}), {
+            'column': 'A',
+            'expression': r'(\w{2})',
+            'newcolumn': 'B',
+        })
+        assert_frame_equal(out, pd.DataFrame({
+            'A': ['abc', 'def'],
+            'B': ['ab', 'de'],
+        }))
 
-        params = {'column': 'stringcol', 'expression': '', 'newcolumn': 'newcol'}
-        out = render(self.table, params)
-        self.assertTrue(out.equals(self.table))  # should NOP when first applied
-
-    def test_string(self):
-        params = {'column': 'stringcol', 'expression': '([\w\s]+)', 'newcolumn': 'newcol'}
-        out = render(self.table.copy(), params)
-        ref = self.table
-        ref['newcol'] = pd.Series(['workbench', 'workbench', 'workbench', 'workbench', 'workbench'])
-        pd.testing.assert_frame_equal(out, ref)
+    def test_no_match_is_nan(self):
+        out = render(pd.DataFrame({'A': ['abc', 'def']}), {
+            'column': 'A',
+            'expression': r'a(\w{2})',
+            'newcolumn': 'B',
+        })
+        assert_frame_equal(out, pd.DataFrame({
+            'A': ['abc', 'def'],
+            'B': ['bc', None],
+        }))
 
     def test_num(self):
         # converts number to strings, NaN if no result?
-        params = {'column': 'floatcol', 'expression': '([\w\s]+)', 'newcolumn': 'newcol'}
-        out = render(self.table.copy(), params)
-        ref = self.table
-        ref['newcol'] = pd.Series(['3', np.nan, np.nan, '10', '0'])
-        pd.testing.assert_frame_equal(out, ref)
+        # TODO [adamhooper, 2018-12-19] delete this feature: require str input
+        out = render(pd.DataFrame({'A': [1, np.nan, 2000.1]}), {
+            'column': 'A',
+            'expression': r'(\d)',
+            'newcolumn': 'B',
+        })
+        assert_frame_equal(out, pd.DataFrame({
+            'A': [1, np.nan, 2000.1],
+            'B': ['1', None, '2'],
+        }))
 
-    def test_num_expect_none(self):
-        # converts number to strings, NaN if no result?
-        params = {'column': 'intcol', 'expression': '([a-z]+)', 'newcolumn': 'newcol'}
-        out = render(self.table.copy(), params)
-        ref = self.table
-        ref['newcol'] = pd.Series([np.nan, np.nan, np.nan, np.nan, np.nan], dtype=object)
-        pd.testing.assert_frame_equal(out, ref)
-
-    def test_cat(self):
-        params = {'column': 'catcol', 'expression': '([\w\s]+)', 'newcolumn': 'newcol'}
-        out = render(self.table.copy(), params)
-        ref = self.table
-        ref['newcol'] = pd.Series(['workbench', 'workbench', 'data', 'data', 'workbench'])
-        pd.testing.assert_frame_equal(out, ref)
-
-    def test_cat_num(self):
-        params = {'column': 'floatcatcol', 'expression': '([\w\s]+)', 'newcolumn': 'newcol'}
-        out = render(self.table.copy(), params)
-        ref = self.table
-        ref['newcol'] = pd.Series(['1', '2', np.nan, '3', '4'])
-        pd.testing.assert_frame_equal(out, ref)
+    def test_categories(self):
+        out = render(pd.DataFrame({'A': ['abc', 'def']}, dtype='category'), {
+            'column': 'A',
+            'expression': r'(\w{2})',
+            'newcolumn': 'B',
+        })
+        expected = pd.DataFrame({
+            'A': ['abc', 'def'],
+            'B': ['ab', 'de'],
+        })
+        # Implementation detail: output is never 'category'.
+        expected['A'] = expected['A'].astype('category')
+        assert_frame_equal(out, expected)
 
     def test_overwrite_col(self):
-        params = {'column': 'stringcol', 'expression': '([\w\s]+)', 'newcolumn': 'stringcol'}
-        out = render(self.table.copy(), params)
-        ref = self.table
-        ref['stringcol'] = pd.Series(['workbench', 'workbench', 'workbench', 'workbench', 'workbench'])
-        pd.testing.assert_frame_equal(out, ref)
+        out = render(pd.DataFrame({'A': ['abc', 'def']}), {
+            'column': 'A',
+            'expression': r'(\w{2})',
+            'newcolumn': 'A',
+        })
+        assert_frame_equal(out, pd.DataFrame({'A': ['ab', 'de']}))
 
-    def test_regex_error(self):
-        # invalid regex expression
-        params = {'column': 'stringcol', 'expression': '#@55[\w\s]+!!!)', 'newcolumn': 'stringcol'}
-        with self.assertRaises(ValueError):
-            render(self.table.copy(), params)
+    def test_invalid_regex_syntax_error(self):
+        out = render(pd.DataFrame({'A': ['a', 'b']}), {
+            'column': 'A',
+            'expression': r'([x)',
+            'newcolumn': 'B',
+        })
+        self.assertEqual(
+            out,
+            'Invalid regex: unterminated character set at position 1'
+        )
 
-        # multiple groups
-        params = {'column': 'stringcol', 'expression': '[\w\s]', 'newcolumn': 'stringcol'}
-        with self.assertRaises(ValueError):
-            render(self.table.copy(), params)
+    def test_invalid_regex_missing_group_captures_all(self):
+        out = render(pd.DataFrame({'A': ['ab', 'cb']}), {
+            'column': 'A',
+            'expression': r'[ab]',
+            'newcolumn': 'B',
+        })
+        self.assertEqual(
+            out,
+            'Your regex needs a capture group. Add (parentheses) around it.'
+        )
+
+    def test_invalid_regex_too_many_groups(self):
+        out = render(pd.DataFrame({'A': ['a', 'b']}), {
+            'column': 'A',
+            'expression': r'([ab])([cd])',
+            'newcolumn': 'B',
+        })
+        self.assertEqual(
+            out,
+            'Workbench only supports one (capture group). Remove '
+            'some parentheses.'
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
